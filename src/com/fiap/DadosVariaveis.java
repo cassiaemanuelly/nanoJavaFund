7a335dd7f055640501842f47f40dca350e4dc788
Cassia Emanuelly <cassiaemanuelly05@gmail.com>
2025-04-30 22:10:15 -0300

    Entrada e saída de dados

---
+++ src/com/fiap/DadosVariaveis.java
@@ -0,0 +1,30 @@
+package com.fiap;
+
+public class DadosVariaveis {
+    public static void main(String[] args) {
+
+        //TIPOS DE DADOS E VARIÁVEIS
+        String nome = "Cassia";
+        int idade = 21;
+        double preco = 10.0;
+        String sexo = "F";
+        boolean filhos = false;
+
+        System.out.println("Nome: "+nome);
+        System.out.println("Idade: "+idade);
+        System.out.println("Valor na conta: "+preco);
+        System.out.println("Sexo: "+sexo);
+        System.out.println("Tem filhos?: "+filhos);
+
+//        O java é case sensitive, ou seja, faz diferenciação entre maiúsculos e minúsculas;
+//
+//        Tirpo de dados
+//        Inteiros: byte, short, int, long;
+//        Pontos fluantes: float double;
+//        Caractere: char;
+//        Array de caracteres: Classe String
+//        boolean: true, false;
+
+        }
+    }
+
